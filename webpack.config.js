cd259d8fe145f7dc88dcd036af403a28deaa8e64
Dimitri Reifschneider <dimitri.reifschneider@gmail.com>
2017-05-29 18:58:30 +0200

    Setup webpack HMR - Installing style and css loader - Adding App component

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,80 @@
-var path = require('path');
+/* global __dirname, require, module */
+
+const webpack = require('webpack');
+const { resolve } = require('path');
 
 module.exports = {
-    entry: './app/index.js',
+    context: resolve(__dirname, 'src'),
+    // putting a context to it
+
+    entry: [
+        'react-hot-loader/patch',
+        // activate HMR for React
+
+        'webpack-dev-server/client?http://localhost:8080',
+        // bundle the client for webpack-dev-server
+        // and connect to the provided endpoint
+
+        'webpack/hot/only-dev-server',
+        // bundle the client for hot reloading
+        // only- means to only hot reload for successful updates
+
+        './index.js'
+        // the entry point of our app
+    ],
+
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        // the output bundle
+
+        path: resolve(__dirname, 'dist'),
+        // destination folder
+
+        publicPath: '/'
+        // necessary for HMR to know where to load the hot update chunks
     },
+
+    devtool: 'inline-source-map',
+
+    devServer: {
+        hot: true,
+        // enable HMR on the server
+
+        contentBase: resolve(__dirname, 'dist'),
+        // match the output path
+
+        publicPath: '/'
+        // match the output `publicPath`
+    },
+
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+            {
+                test: /\.jsx?$/,
+                use: [ 'babel-loader', ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader?modules',
+                        options: {
+                            modules: true,
+                            localIdentName: '[name]__[local]--[hash:base64:5]'
+                        }
+                    }
+                ],
+            },
         ]
-    }
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        // enable HMR globally
+
+        new webpack.NamedModulesPlugin(),
+        // prints more readable module names in the browser console on HMR updates
+    ]
 };
